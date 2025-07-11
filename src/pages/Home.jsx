df94923fbdd7cf53070567f446413d3555cb7454
SonaSonali <104508809+SonaSonali@users.noreply.github.com>
2025-07-11 22:29:23 +0530

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,27 @@
+import { AboutSection } from "../components/AboutSection";
+import { ContactSection } from "../components/ContactSection";
+import { HeroSection } from "../components/HeroSection";
+import { Navbar } from "../components/Navbar";
+import { ProjectsSection } from "../components/ProjectsSection";
+import { SkillsSection } from "../components/SkillsSection";
+import { ThemeToggle } from "../components/themeToggle";
+
+export const Home = () => {
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      {/* theme toggle */}
+      <ThemeToggle />
+
+      {/* Navbar */}
+      <Navbar />
+      {/* Main Content */}
+      <main>
+        <HeroSection />
+        <AboutSection />
+        <SkillsSection />
+        <ProjectsSection />
+        <ContactSection />
+      </main>
+    </div>
+  );
+};
